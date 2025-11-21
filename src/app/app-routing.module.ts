f98ddb8eb3dc043435404d4c7cfd1a89105bc5d6
Mohammad Fareed <fareed543@gmail.com>
2025-11-21 17:33:19 +0530

    feature : about salahtime page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   },
   {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+  {
     path: 'privacy-policy',
     loadChildren: () => import('./privacy-policy/privacy-policy.module').then(m => m.PrivacyPolicyModule)
   }
