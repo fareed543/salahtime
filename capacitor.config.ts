ac8839fa8d9a12dce500f7603fe92e4653408169
Mohammad Fareed <fareed543@gmail.com>
2025-11-21 10:38:54 +0530

    feature: Android App

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fareed.salahtime',
+  appName: 'salahtime',
+  webDir: 'dist/salahtime'
+};
+
+export default config;
