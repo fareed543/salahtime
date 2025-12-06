20c8bbc93ff30784d24f18e3e4aa9304fc683f38
Mohammad Fareed <fareed543@gmail.com>
2025-12-07 02:12:04 +0530

    Feature : Settings Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,25 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   },
-  {
+   {
+    path: 'qibla',
+    loadChildren: () => import('./qibla/qibla.module').then(m => m.QiblaModule)
+  },
+   {
     path: 'about',
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
   },
   {
     path: 'privacy-policy',
     loadChildren: () => import('./privacy-policy/privacy-policy.module').then(m => m.PrivacyPolicyModule)
-  }
+  },
+  
+ 
+  
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule)
+  },
 ];
 
 @NgModule({
