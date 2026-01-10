85e99e423d8f7ea44bd8ed67ee8d48b17685d82c
Mohammad Fareed <fareed543@gmail.com>
2026-01-10 23:10:14 +0530

    Location changes

--- build-apk.bat
+++ build-apk.bat
@@ -6,7 +6,7 @@ echo Starting SalahTime Build - %date% %time%
 echo ==========================================
 
 :: Go to project folder
-cd /d D:\salah-time-board
+cd /d D:\apps\salah-time
 
 :: Remove old dist folder
 if exist dist (
