5c0bdea807ccbff2b5f9be697b10808a7b5ed0d5
Mohammad Fareed <fareed.m@intellectdesign.com>
2026-01-28 11:20:46 +0530

    1.0.36
    Ramzan 2026 Schedule

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   appName: 'SalahTime',
-  appVersion: '1.0.35',
+  appVersion: '1.0.36',
   production: false,
   notificationChannelId: 'salah_channel',
   notificationChannelName: 'SalahTime Notifications'
