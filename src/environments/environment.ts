72ec806d92f01c2829c33008921f28a7eb9d1418
Mohammad Fareed <fareed543@gmail.com>
2026-02-01 01:41:20 +0530

    1.0.38

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   appName: 'SalahTime',
-  appVersion: '1.0.36',
+  appVersion: '1.0.38',
   production: false,
   notificationChannelId: 'salah_channel',
   notificationChannelName: 'SalahTime Notifications'
