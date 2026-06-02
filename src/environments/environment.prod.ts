7bb4be8c71fe25e9e1ed0a1706a151117291fb56
Mohammad Fareed <fareed543@gmail.com>
2026-06-03 01:12:59 +0530

    Web & Mobile App

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  appName: 'Salah Time',
-  appVersion: '1.0.39',
-  production: true,
-  apiUrl: 'https://walletplus.in/',
-  notificationChannel: 'salahTime',
-  notificationChannelName: 'SalahTime Notifications'
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  appName: 'Salah Time',
-  appVersion: '1.0.39',
-  production: false,
-  apiUrl: 'https://walletplus.in/',
-  notificationChannelId: 'salah_channel',
-  notificationChannelName: 'SalahTime Notifications'
-};
